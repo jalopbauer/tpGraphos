2b6f11547d683bc7ba87e068f7b4fcbae6ae55c7
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-26 14:42:35 -0300

    StringGraphBuilder

--- src/test/java/directed_graph/StringGraphBuilder.java
+++ src/test/java/directed_graph/StringGraphBuilder.java
@@ -3,11 +3,12 @@ package directed_graph;
 
 import graph.Edge;
 import graph.Graph;
-import org.junit.Test;
 
 import java.util.List;
 
-public class VertexPersistenceTest {
+public class StringGraphBuilder {
+
+
 
     void buildGraph(List<String> vertexes, List<Edge<String>> edges, Graph<String> graph) {
         for (String vertex : vertexes) graph.addVertex(vertex);
