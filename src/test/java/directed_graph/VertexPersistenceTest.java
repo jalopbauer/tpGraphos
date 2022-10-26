0361fbca74287a25a99e0670e5529998851dcd9e
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-26 14:30:36 -0300

    buildGraph method

--- src/test/java/directed_graph/VertexPersistenceTest.java
+++ src/test/java/directed_graph/VertexPersistenceTest.java
@@ -1,8 +1,16 @@
 package directed_graph;
 
 
+import graph.Edge;
+import graph.Graph;
 import org.junit.Test;
+
+import java.util.List;
 
 public class VertexPersistenceTest {
 
+    void buildGraph(List<String> vertexes, List<Edge<String>> edges, Graph<String> graph) {
+        for (String vertex : vertexes) graph.addVertex(vertex);
+        for (Edge<String> edge : edges) graph.addEdge(edge);
+    }
 }
