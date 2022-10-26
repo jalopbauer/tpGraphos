9bab2a555cc794146f15d78e9a18dd3e984ab60f
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-26 14:46:26 -0300

    emptyGraph

--- src/test/java/directed_graph/StringGraphBuilder.java
+++ src/test/java/directed_graph/StringGraphBuilder.java
@@ -4,11 +4,15 @@ package directed_graph;
 import graph.Edge;
 import graph.Graph;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StringGraphBuilder {
 
 
+    void emptyGraph(Graph<String> graph) {
+        buildGraph(new ArrayList<>(), new ArrayList<>(), graph);
+    }
 
     void buildGraph(List<String> vertexes, List<Edge<String>> edges, Graph<String> graph) {
         for (String vertex : vertexes) graph.addVertex(vertex);
