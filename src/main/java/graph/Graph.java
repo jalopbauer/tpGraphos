cc28bea1c3ae553e13765be1c5ea80a221f2cb91
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-10-26 11:05:27 -0300

    Project restructured

---
+++ src/main/java/graph/Graph.java
@@ -0,0 +1,15 @@
+package graph;
+
+import java.util.List;
+
+public interface Graph<T> {
+    int addVertex(T x);
+    void addEdge(Edge edge);
+    void deleteEdge(Edge edge);
+    void deleteVertex(int v);
+    boolean existsEdge(Edge edge);
+    int orden();
+    int alpha();
+    T getVertex(int v);
+    List<T> getAdyListForVertex(int v);
+}
