7da414b5dd1d4e940ba690a71e0c488539c2e989
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-11-15 12:37:33 -0300

    Warshall

--- src/main/java/graph/methods/Warshall.java
+++ src/main/java/graph/methods/Warshall.java
@@ -11,7 +11,14 @@ public class Warshall<T> {
         boolean[][] adyMatrix =  new boolean[graph.order()][graph.order()];
         List<T> vertexes = new ArrayList<>(graph.getVertexes());
         populate(graph, adyMatrix);
-
+        for (int k = 0; k < vertexes.size(); k++) {
+            for (int i = 0; i < vertexes.size(); i++) {
+                for (int j = 0; j < vertexes.size(); j++) {
+                    if (adyMatrix[i][k] && adyMatrix[k][j])
+                        adyMatrix[i][j] = true;
+                }
+            }
+        }
         return adyMatrix;
     }
 
